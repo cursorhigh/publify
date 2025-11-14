bd1ea3bd7ea1928a2ab054d5f485a4fe4aa65a69
TheHackerClown <dhruvprataps14@gmail.com>
2025-11-14 23:36:48 +0530

    Added Custom Callback

--- prototype_backend/app.py
+++ prototype_backend/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from model_loader import load_translation_model
 from utils import translate_text
 from region_map import REGION_TO_LANG
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -32,3 +33,44 @@ def translate(req: TranslateRequest):
         "target_language": tgt_lang,
         "translated": result
     }
+
+
+# for Agora Conversational AI integration
+@app.post("/publify_translate_callback")
+async def publify_translate_callback(req: Request):
+    
+    data = await req.json() # Parse JSON body
+
+    user_msg = data["messages"][-1]["content"] # Get the last message content
+
+    # Safeguard: Ensure user_msg is in English, as Agora ASR can give non-English text
+    english_text = translate_text(user_msg, "eng_Latn", model, tokenizer, device)
+
+    # Determine target language from user_lang field
+    sanitized_lang = data.get("user_lang", "hi_IN").replace("-", "_")
+
+    final_translation = translate_text(
+        english_text,
+        sanitized_lang,
+        model,
+        tokenizer,
+        device
+    )
+
+    #OpenAI Result Format
+    return {
+        "id": str(uuid4()),
+        "object":"chat.completion",
+        "choices":[
+            {
+                "index":0,
+                "message":{
+                    "role":"publify_agent",
+                    "content": final_translation
+                },
+                "finish_reason":"stop"
+            }
+        ]
+
+    }
+
